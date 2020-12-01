5670adc7963d476258f34f2be6206b8173b4f860
Pavel Tsytovich <pavel.tsytovich@gmail.com>
2020-12-01 10:13:58 +0300

    Setting up GitHub Classroom Feedback

--- TDDKata/Calculator.cpp
+++ TDDKata/Calculator.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "string.h"
 #include "Calculator.h"
 
 int Calculator::Add(std::string expression)
@@ -8,5 +9,33 @@ int Calculator::Add(std::string expression)
 
 int Calculator::Add(char* expression)
 {
-    throw 0;
+    // Empty
+    if (strlen(expression) == 0) return 0;
+    // Few parameters  
+    if (strlen(expression) == 1) return 0;
+    if (strlen(expression) == 2) return 0;
+    // Munis
+    if (strchr(expression, '-')) return 0;
+    // Format error
+    if (strchr(expression, ',') == NULL) return 0;
+    // First param error 
+    if(expression[0] == ',') return 0;
+ 
+    int firstNumber;
+    int secondNumber;
+
+    // One
+    if (expression[1] == ',')
+    {
+        firstNumber = atoi(expression + 0);
+        secondNumber = atoi(expression + 2);
+    }
+    // Two
+    else if (expression[2] == ',')
+    {
+        firstNumber = atoi(expression + 0);
+        secondNumber = atoi(expression + 3);
+    }
+
+    return firstNumber + secondNumber;
 }
--- TDDKata/test.cpp
+++ TDDKata/test.cpp
@@ -1,5 +1,108 @@
 #include "pch.h"
-	TEST(TDDKata, TestName) {
-		ASSERT_EQ(1, 1);
-		ASSERT_TRUE(true);
-	}
+#include "string.h"
+#include "Calculator.h"
+
+TEST(TDDKata, AddTestEmptyStr)
+{
+	// Arrange
+	char str[] = "";
+	int  exp = 0;
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+TEST(TDDKata, AddTestMinus)
+{
+	// Arrange
+	char str[] = "-";
+	int  exp = 0;
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+
+TEST(TDDKata, AddTesFormatOk)
+{
+	// Arrange
+	char str[] = ",";
+	int  exp = 0;
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+TEST(TDDKata, AddTestFirstParam)
+{
+	// Arrange
+	char str[] = ",1";
+	int  exp = 0;
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+TEST(TDDKata, AddTestSecondParam)
+{
+	// Arrange
+	char str[] = "1,";
+	int  exp = 0;
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+TEST(TDDKata, AddTestOneNumber)
+
+{
+	// Arrange
+	char str[] = "1,1";
+	int  exp = atoi(str + 0) + atoi(str + 2);
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+TEST(TDDKata, AddTestTwoNumber)
+{
+	// Arrange
+	char str[] = "11,11";
+	int  exp = atoi(str+0) + atoi(str+3);
+
+	Calculator myCalc;
+
+	// Action
+	int actual = myCalc.Add(str);
+
+	// Assert
+	ASSERT_EQ(exp, actual);
+}
+
+
+
+
